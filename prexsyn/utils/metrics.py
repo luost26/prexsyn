d36872d84c02da687d4cb6ef2e32cb083c9cc956
Shitong Luo <luost26@gmail.com>
2026-03-31 17:25:59 -0400

    add similarity functions

---
+++ prexsyn/utils/metrics.py
@@ -0,0 +1,23 @@
+import torch
+import numpy as np
+
+
+def _tanimoto_similarity_torch(x1: torch.Tensor, x2: torch.Tensor):
+    intersection = torch.min(x1, x2).sum(dim=-1)
+    union = torch.max(x1, x2).sum(dim=-1)
+    return intersection / (union + 1e-8)
+
+
+def _tanimoto_similarity_numpy(x1: np.ndarray, x2: np.ndarray):
+    intersection = np.minimum(x1, x2).sum(axis=-1)
+    union = np.maximum(x1, x2).sum(axis=-1)
+    return intersection / (union + 1e-8)
+
+
+def tanimoto_similarity(x1: torch.Tensor | np.ndarray, x2: torch.Tensor | np.ndarray):
+    if isinstance(x1, torch.Tensor) and isinstance(x2, torch.Tensor):
+        return _tanimoto_similarity_torch(x1, x2)
+    elif isinstance(x1, np.ndarray) and isinstance(x2, np.ndarray):
+        return _tanimoto_similarity_numpy(x1, x2)
+    else:
+        raise ValueError("Input types must be both torch.Tensor or both np.ndarray.")
