ddad1ef92f7c164f8839989f7361a333d2ef12cd
luost26 <45375360+luost26@users.noreply.github.com>
2026-04-21 16:48:06 -0400

    Apply linting (#9)
    * trigger
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove pyright hook from .pre-commit-config.yaml
    
    Removed pyright hook from pre-commit configuration
    
    * add pyright workflow
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- prexsyn/models/embeddings/__init__.py
+++ prexsyn/models/embeddings/__init__.py
@@ -1,6 +1,8 @@
 from .base import Embedding as Embedding
 from .descriptor import (
     DescriptorEmbedder as DescriptorEmbedder,
+)
+from .descriptor import (
     DescriptorEmbedderConfig as DescriptorEmbedderConfig,
 )
 from .synthesis import SynthesisEmbedder as SynthesisEmbedder
